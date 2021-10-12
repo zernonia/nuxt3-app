cbfa55803207dc45a19b827eea135e3f65dc35d6
zernonia <59365435+zernonia@users.noreply.github.com>
2021-10-12 22:44:28 +0800

    initial push

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="bg-dark-300">
+      <h1 class="py-0 my-0 text-4xl font-bold">Hi</h1>
+      <input type="text" v-model="name" placeholder="name" class="text-2xl" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const name = ref("123")
+</script>
