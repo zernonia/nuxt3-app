39adf07c430f89a621597ddb40257d35de56a80a
zernonia <59365435+zernonia@users.noreply.github.com>
2021-10-12 22:54:21 +0800

    update: remove unneccessary stuff

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@
     <div class="bg-dark-300">
       <h1 class="py-0 my-0 text-4xl font-bold">Hi</h1>
       <input type="text" v-model="name" placeholder="name" class="text-2xl" />
+      <p>
+        {{ name }}
+      </p>
     </div>
   </div>
 </template>
